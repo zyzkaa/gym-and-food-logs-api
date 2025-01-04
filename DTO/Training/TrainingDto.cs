18b86d46cce24e8a822418959e4bdb58cd952439
zyzka <zuziaswis@gmail.com>
2025-01-04 15:03:19 +0100

    feat: added data annotations in classes

--- DTO/Training/TrainingDto.cs
+++ DTO/Training/TrainingDto.cs
@@ -6,10 +6,15 @@ namespace WebApp.DTO;
 
 public class TrainingDto
 {
+    [Required(ErrorMessage = "Name is required")]
+    [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters")]
     public string Name { get; set; }
+        
+    [Required(ErrorMessage = "Date is required")]
     public DateOnly Date { get; set; }
+    
     public TimeSpan? Duration { get; set; } = TimeSpan.Zero;
-
+    
     public record StrParamsDto(int set, int weight, int repetitions);
     public record StrengthExerciseDto(int ExerciseId, IEnumerable<StrParamsDto> StrParams);
     public ICollection<StrengthExerciseDto> StrExercisesDto { get; set; }
--- obj/Debug/net9.0/WebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77237bff0839183d0688a46cfbd07b28b751052")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55503041fbec0c1453510a20fc10cd83b71df039")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
