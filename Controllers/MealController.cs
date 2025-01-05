5c48b2022590b121994a1e1902ea6c1c695dc33a
munio7 <maciej.wrotniak03@gmail.com>
2025-01-05 17:59:56 +0100

    Dodano Utility dict Dodalem parsowanie na mealDto

--- Controllers/MealController.cs
+++ Controllers/MealController.cs
@@ -3,6 +3,7 @@ using WebApp.DTO;
 using WebApp.DTO.Meals;
 using WebApp.Entities;
 using WebApp.Services.MealServices;
+using WebApp.Utill.Meals;
 
 namespace WebApp.Controllers;
 
@@ -21,38 +22,33 @@ public class MealController : ControllerBase
     public async Task<IActionResult> AddMeal([FromBody] MealDto mealDto)
     {
         var meal = await _mealService.AddMeal(mealDto);
-         var mealDtoResponse = new MealDto
-            {
-                Id = meal.Id,
-                Name = meal.Name,
-                Ingredients = meal.Ingredients.Select(i => new MealIngredientDto
-                {
-                    ProductId = i.Product.Id,
-                    Quantity = i.Quantity
-                }).ToList()
-            };
-        return CreatedAtAction(nameof(GetMealById), new { id = mealDtoResponse.Id }, mealDtoResponse);
+        var mealDtoResponse = MealUtillity.ParseToMealDto(meal);
+        return Ok(mealDtoResponse);
     }
 
     [HttpGet("get_all")]
     public async Task<IActionResult> GetMeals()
     {
         var meals = await _mealService.GetMeals();
-        
-        return Ok(meals);
+        var mealsDtoResponse = meals.Select(m => MealUtillity.ParseToMealDto(m)).ToList();
+        return Ok(mealsDtoResponse);
     }
 
     [HttpGet("get_by_id/{id}")]
     public async Task<IActionResult> GetMealById(int id)
     {
         var meal = await _mealService.GetMealById(id);
-        return Ok(meal);
+        MealDto mealDtoResponse = MealUtillity.ParseToMealDto(meal);
+        return Ok(mealDtoResponse);
     }
 
     [HttpDelete("delete/{id}")]
     public async Task<IActionResult> DeleteMealById(int id)
     {
         var meal = await _mealService.DeleteMealById(id);
+        var mealDtoResponse = MealUtillity.ParseToMealDto(meal);
         return Ok(meal);
     }
+
+
 }
---
+++ Utill/Meals/MealUtillity.cs
@@ -0,0 +1,22 @@
+﻿using WebApp.DTO.Meals;
+using WebApp.Entities;
+
+namespace WebApp.Utill.Meals;
+
+public static class MealUtillity
+{
+    public static MealDto ParseToMealDto(Meal meal)
+    {
+        MealDto mealDto = new MealDto()
+        {
+            Id = meal.Id,
+            Name = meal.Name,
+            Ingredients = meal.Ingredients.Select(I => new MealIngredientDto
+            {
+                ProductId = I.Product.Id,
+                Quantity = I.Quantity
+            }).ToList()
+        };
+        return mealDto;
+    }
+}
