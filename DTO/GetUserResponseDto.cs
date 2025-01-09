104db2ae802e956ca25765015c12a652354939db
zyzka <zuziaswis@gmail.com>
2025-01-09 17:54:17 +0100

    uwagi nt get user response dto

--- DTO/GetUserResponseDto.cs
+++ DTO/GetUserResponseDto.cs
@@ -2,11 +2,12 @@
 
 namespace WebApp.DTO;
 
+// i moze jakosz zmiana do nazwy typu user with details dto bo juz jeden dto do usera jest
 public class GetUserResponseDto
 {
     public int Id { get; set; }
     public string Username { get; set; }
-    public string Password { get; set; }
+    public string Password { get; set; } // imo do wywalenia bo sie nie powinno zwacac hasla :/
     public int Weight { get; set; }
     public int Height { get; set; }
     public int Age { get; set; }
