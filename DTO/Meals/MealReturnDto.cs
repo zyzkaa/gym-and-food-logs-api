8cf08ae36ad4561194490971ee92eb6ace3e7e6a
munio7 <maciej.wrotniak03@gmail.com>
2025-01-15 15:20:22 +0100

    last v3

---
+++ DTO/Meals/MealReturnDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebApp.DTO.Meals;
+
+public class MealReturnDto
+{
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public int? Id { get; set; }
+    public string Name { get; set; }
+    public string Calories { get; set; }
+    
+    [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+    public List<string> Ingredients { get; set; }
+}
